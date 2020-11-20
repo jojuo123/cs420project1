e2227a2aaaff6305970bd9d8feda7257bab83e25
Nhat Hoang <npnhoang18@apcs.vn>
2020-11-20 22:26:26 +0700

    fix syntax errors

--- Hider.py
+++ Hider.py
@@ -2,7 +2,8 @@ import Agent as ag
 
 class Hider(ag.Agent):
     def __init__(self, position, sight):
-        ag.__init__(self, position, sight)
+        # ag.__init__(self, position, sight)
+        super().__init__(position, sight)
     
     def Dead(self):
         self.position = [-1, -1]
--- Seeker.py
+++ Seeker.py
@@ -2,7 +2,8 @@ import Agent as ag
 
 class Seeker(ag.Agent):
     def __init__(self, position, sight):
-        ag.__init__(self, position, sight)
+        # ag.__init__(self, position, sight)
+        super().__init__(position, sight)
     
     def move(self, environment, announceArray, visionArray):
         pass
